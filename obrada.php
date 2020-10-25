d7097962e07212c1b54307c772c471e36a86ac0a
tamilov3 <tamaranaumovic@gmail.com>
2020-10-26 00:01:44 +0100

    kreiran post novosti i brisanje

---
+++ obrada.php
@@ -0,0 +1,27 @@
+<?php
+    include "Database.php";
+    $mydb = new Database('rest');
+    if(isset($_POST["posalji"]) && $_POST["posalji"]="Posalji zahtev"){
+        if($_POST["naslov_novosti"]!=null && $_POST["tekst_novosti"]!=null && $_POST["kategorija_odabir"]!=null){
+            $niz = ["naslov"=> "'".$_POST["naslov_novosti"]."'", "tekst"=>"'".$_POST["tekst_novosti"]."'", "datumvreme"=>"NOW()", "kategorija_id"=>$_POST["kategorija_odabir"]];
+            if($mydb->insert("novosti", "naslov, tekst, datumvreme, kategorija_id", $niz)){
+                echo "vrednosti ubacene";
+            }else{
+                echo "vrednosti nisu ubacene";
+            }
+            $_POST = array();
+            exit();
+        }elseif($_POST["brisanje"]!=null && $_POST["odabir_tabele"]!=null){
+            $tabela = $_POST["odabir_tabele"];
+            $id = "id";
+            $id_val = $_POST["brisanje"];
+            if($mydb->delete($tabela,$id,$id_val)){
+                echo "red obrisan";
+            }else{
+                echo "greska prilikom brisanja";
+            }
+            $_POST = array();
+            exit();
+        }
+    }
+?>
